26d1d36bef087ed2f294ec8affc3a7a9af951cb5
Thamizharasu S <zarub2k@gmail.com>
2017-12-18 20:14:44 +0530

    GetResponse handler added

--- src/main/java/com/hustlebar/hustic/get/GetResponseHandler.java
+++ src/main/java/com/hustlebar/hustic/get/GetResponseHandler.java
@@ -13,15 +13,19 @@ public class GetResponseHandler implements ResponseHandler<GetResponse> {
     public GetResponse handleResponse(HttpResponse httpResponse) throws IOException {
         int code = httpResponse.getStatusLine().getStatusCode();
         GetResponse getResponse = new GetResponse(code);
-
         if (code != 200) {
 
         }
 
         InputStream content = httpResponse.getEntity().getContent();
         JsonObject responseJson = Json.createReader(content).readObject();
-        getResponse.setData(responseJson);
+        boolean isFound = responseJson.getBoolean("found");
+        if (!isFound) {
+            return getResponse;
+        }
 
+
+        getResponse.setData(responseJson.getJsonObject("_source"));
         return getResponse;
     }
 }
