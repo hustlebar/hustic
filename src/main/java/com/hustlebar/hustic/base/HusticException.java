85659772776d4e7b05a163d2713e71139b0def31
Thamizharasu <zarub2k@gmail.com>
2017-12-20 23:38:01 +0530

    Exception code is added

--- src/main/java/com/hustlebar/hustic/base/HusticException.java
+++ src/main/java/com/hustlebar/hustic/base/HusticException.java
@@ -1,4 +1,18 @@
 package com.hustlebar.hustic.base;
 
 public class HusticException extends Exception {
+    private int code = 500;
+
+    public HusticException() {
+        super("Error while communicating to the server");
+    }
+
+    public HusticException(int code, String message) {
+        super(message);
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
 }
